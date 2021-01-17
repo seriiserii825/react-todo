1c99861aaa388bf67cb6f5b69a3bf8cea202c57b
serii <seriiburduja@gmail.com>
2021-01-17 15:35:09 +0200

    state

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,22 +1,51 @@
 import React from "react";
 import './todo-list-item.scss';
 
-class TodoListItem extends React.Component {
+export default class TodoListItem extends React.Component {
+  state = {
+    done: false,
+    important: false
+  };
+  onLabelClick = () => {
+    this.setState((state) => {
+      return {
+        done: !state.done
+      };
+    });
+  }
+  onMarkImportant = () => {
+    this.setState((state) => {
+      return {
+        important: !state.important
+      };
+    });
+  }
+  
   render () {
-    const {label, important = false} = this.props;
-    const liStyleColor = important ? 'tomato' : 'initial';
-    const liStyle = {'color': liStyleColor};
+    const {label} = this.props;
+    const {done, important} = this.state;
+    let classNames = 'list-group-item todo-list-item';
+    if (done) {
+      classNames += ' done';
+    }
+    if (important) {
+      classNames += ' important';
+    }
     return (
-      <li className="list-group-item todo-list-item">
-        <span className="todo-list-item" style={liStyle}>{label}</span>
-        <button className="btn btn--important btn-outline-success">
+      <li className={classNames}>
+        <span onClick={this.onLabelClick} className="todo-list-item__text">{label}</span>
+        <button
+          className="btn btn--important btn-outline-success"
+          onClick={this.onMarkImportant}
+        >
           <i className="fa fa-exclamation"/>
         </button>
-        <button className="btn btn--delete btn-outline-danger">
+        <button
+          className="btn btn--delete btn-outline-danger"
+        >
           <i className="fa fa-trash"/>
         </button>
       </li>
     )
   }
 }
-export default TodoListItem;
