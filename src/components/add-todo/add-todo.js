53dd1605207c273a6a27fb1897ffb5c547e4b229
serii <seriiburduja@gmail.com>
2021-01-18 20:06:06 +0200

    add item with input

--- src/components/add-todo/add-todo.js
+++ src/components/add-todo/add-todo.js
@@ -1,11 +1,34 @@
 import React from "react";
 import './add-todo.scss';
 
-const AddTodo = ({onAdd}) => {
-  return (
-    <div className="add-todo">
-      <button onClick={() => onAdd('hello world')} className="btn btn-primary">Add todo</button>
-    </div>
-  )
+export default class AddTodo extends React.Component {
+  state = {
+    label: ''
+  };
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    });
+  }
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onAdd(this.state.label);
+    this.setState({
+      label: ''
+    });
+  }
+  
+  render () {
+    return (
+      <form className="add-todo" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          onChange={this.onLabelChange}
+          placeholder="Ad new task todo..."
+          value={this.state.label}
+        />
+        <button className="btn btn-primary">Add todo</button>
+      </form>
+    )
+  }
 }
-export default AddTodo;
